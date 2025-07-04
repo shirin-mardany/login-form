8dc33ef48615b413e4a6bd71e8c9ed2dff12074c
shirin <shirin.mardany@gmail.com>
2025-07-04 18:31:47 +0430

    dark and light

--- script-first.js
+++ script-first.js
@@ -5,23 +5,39 @@ const passwordInput = document.querySelector("#password");
 const emailInput = document.querySelector("#email");
 console.log(blobEl);
 
+loginformEl.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  try {
+    const res = await fetch(`https://fakestoreapi.com/auth/login`, {
+      method: "POST",
+      body: JSON.stringify({
+        username: emailInput.value,
+        password: passwordInput.value,
+      }),
+    });
+    // creat json file from data for use that >>
+    const data = await res.json();
+    loginformEl.classList.add("hide");
+    blobEl.textContent = data.token;
+  } catch (err) {
+    alert("noooooooooooooooooooooo");
+  }
+});
 
-loginformEl.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    try {
-      const res = await fetch(`https://fakestoreapi.com/auth/login`, {
-        method: "POST",
-        body: JSON.stringify({
-          username: emailInput.value,
-          password: passwordInput.value,
-        }),
-      });
-        // creat json file from data for use that >>
-      const data = await res.json()
-      loginformEl.classList.add("hide")
-      blobEl.textContent = data.token
-        
-    } catch (err) {
-      alert('noooooooooooooooooooooo')
-    }
-})
+// start dark and light ----------------------------------------- >>
+const changeColor = document.querySelector("#changeColor");
+const root = document.querySelector(":root");
+let isDark = true;
+// console.log(root);
+
+changeColor.addEventListener("change", () => {
+  if (isDark) {
+    console.log("befor");
+    root.style.setProperty("--color-1-1f1f47", "#656d4a");
+    isDark = false;
+  } else {
+    console.log("after");
+    root.style.setProperty("--color-1-1f1f47", "#1f2c47");
+    isDark = true;
+  }
+});
